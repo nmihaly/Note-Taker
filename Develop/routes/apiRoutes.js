cfaecd69d494a01b456ba864b2fb8e43d7a8db14
Your Name <nickmihaly@gmail.com>
2021-07-02 19:20:48 -0700

    updated post api route

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,12 +1,38 @@
-const path = require('path');
 const router = require('express').Router();
+const { db } = require('../../Develop/db/db.json');
+const fs = require('fs');
+const util = require('util');
 
-router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/index.html'));
+function createNewNote(body, notesArray) {
+  const newNote = body;
+  if (!Array.isArray(notesArray))
+      notesArray = [];
+  
+  if (notesArray.length === 0)
+      notesArray.push(0);
+
+  body.id = notesArray[0];
+  notesArray[0]++;
+
+  notesArray.push(newNote);
+  fs.writeFileSync(
+      path.join(__dirname, db),
+      JSON.stringify(notesArray, null, 2)
+  );
+  return newNote;
+}
+
+router.get('/api/notes'), (req, res) => {
+  let data = json.parse(fs.readFileSync(db, 'utf-8'));
+  console.log(json.stringify(data));
+  res.json(data);
+  }
+
+ 
+router.post('/api/notes', (req, res) => {
+  const newNote = createNewNote(req.body, db);
+    res.json(newNote);
 });
 
-router.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, '../public/notes.html'));
-});
 
 module.exports = router;
